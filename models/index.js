b5e5b2727509fa026547f8f8e7760489e8e1dd1d
inmyblue <inmyblue0930@naver.com>
2022-03-22 12:39:08 +0900

    환경변수 수정

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const connect = () => {
     const mongooseId = process.env.MONGO_ID
     const mongoosePw = process.env.MONGO_PWD
-    mongoose.connect("mongodb://mongooseId:mongoosePw@localhost:27017/board", {ignoreUndefined : true}).catch((err)=>{
+    mongoose.connect(`mongodb://${mongooseId}:${mongoosePw}@localhost:27017/board`, {ignoreUndefined : true}).catch((err)=>{
         console.error(err)
     })
     console.log("mongoDB connected")
