d9c0a1d7462890f5984a78d2a1fa463d1107a869
inmyblue <98289614+inmyblue@users.noreply.github.com>
2022-04-09 09:49:21 +0900

    SSL

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require("express")
-const routes = require("./routes")
-const connect = require("./models")
-const cors = require("cors")
-const cookieParser = require("cookie-parser")
-const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./swagger-output");
+const express = require('express')
+const routes = require('./routes')
+const connect = require('./models')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger-output')
 const app = express()
 
 // env 불러오기
-require('dotenv').config();
+require('dotenv').config()
 
 // ejs setting
-app.set('views',__dirname+'/views')
-app.set('view engine','ejs')
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 app.engine('html', require('ejs').renderFile)
 
 //MongoDB Connection
@@ -20,15 +20,17 @@ connect()
 
 //MiddleWare
 app.use(express.static('views'))
+app.use(express.static('.well-known'))
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded())
-app.use((req, res, next)=>{ //x-Powerd-By 제거
-    res.removeHeader("X-Powered-By");
-    next();
-});
+app.use((req, res, next) => {
+	//x-Powerd-By 제거
+	res.removeHeader('X-Powered-By')
+	next()
+})
 app.use(cors())
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/', routes)
 
--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const app = require('./app')
-const PORT = 3000
-const handleListening = () =>
-	console.log(`Listening on : http://localhost:${PORT}`)
+const fs = require('fs')
+const https = require('https')
+const domain = process.env.DOMAIN
 
-app.listen(PORT, handleListening())
+const privateKey = fs.readFileSync(
+	`/etc/letsencrypt/live/${domain}/privkey.pem`,
+	'utf8'
+)
+const certificate = fs.readFileSync(
+	`/etc/letsencrypt/live/${domain}/cert.pem`,
+	'utf8'
+)
+const ca = fs.readFileSync(`/etc/letsencrypt/live/${domain}/chain.pem`, 'utf8')
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca,
+}
+
+const httpsServer = https.createServer(credentials, app)
+
+// const PORT = 3000
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443')
+})
+// app.listen(PORT, handleListening())
